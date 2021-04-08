ffa66a66d389a63266145fda35ef43d923d7c38d
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-04-08 21:53:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
-
-API_ID = int(getenv("API_ID")
-API_HASH = getenv("API_HASH")
-
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-
 
 SESSION_NAME= "BQAyd3DsG4YsTe8yWZYnWJDeZY_E5QfQVEaVAHcDHeFXICJPSwB1iKOfruhEG5ASfymtkBQagr85P1rhkFXlaUVzKAYIeCOt0wklQJY3twDzWV4KcAgORiFp4M9bBVfZI7JaffLGd6-LODNN6PSn_9DTH5qTqKP5f-AusIRXNzQi_IJaVfWBV1pmf9M20lrexnFgucwctq4PUDbRaZ1d8mADAvWMKzliiY6UGWVhgrnpmVTUaCUS0Al9ksWm8psqGy3S6tVZPLErPS3ATpOF43BcJMt98685MutycMf7h9crsUNNNIseV6KMH-8yuWZPjagVDlnR5tZuPUH-yVDiTwMbPtmoUAA"
 BOT_TOKEN= "1556849815:AAGyafjOM5BOEsPXRrTWf0YouMUhdcaUo0E"
