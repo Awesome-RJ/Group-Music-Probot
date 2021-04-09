39fdfc44146f0b0f264f32a3c044d7b561a154f3
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-04-09 11:17:52 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @Infinity_BOTs**'
+        rep = '**🎵 Uploaded by @Group_Music_Probot...**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
