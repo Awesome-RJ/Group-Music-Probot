7d14e76c710f0fa936148cae1a233de06e759946
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-04-24 12:03:13 +0530

    [batch] Comments and minor edits for batch files

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -20,7 +20,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "DaisyMusic"
+        user.first_name =  "Group_MusicProbot"
 
     try:
         await USER.join_chat(invitelink)
